3f809940a36afc9b077fd449976b9c12866328cc
nora bernhardt <noraghasel@gmail.com>
2022-03-01 18:26:44 +0100

    Add datetime version

--- script.py
+++ script.py
@@ -1,17 +1,30 @@
 
 import csv
-import datetime
+from datetime import date
+from datetime import datetime
+
+
+def currentAge(birthday):
+    today=date.today()
+    yearDiff = today.year - birthday.year
+    noBirthdayThisYear = (today.month, today.day) < (birthday.month, birthday.day)
+
+    if noBirthdayThisYear:
+        return yearDiff -1
+    return yearDiff
 
 name=input("Wie heißt du? ")
-age=input ("Wie lautet dein Geburtsdatum? ")
+age=input("Wie lautet dein Geburtsdatum? YYYY.MM.DD ")
+
 
 with open("data.csv", "a") as f:
-    studentwriter=csv.writer(f)
-    x = int(age)
-    if x <= 18:
-        print("Du bist zu jung für die Liste")
-    else:
+    studentwriter=csv.writer(f) 
+    birthday=datetime.strptime(age, "%Y.%m.%d")   
+    
+    if currentAge(birthday) > 18:
         studentwriter.writerow([name, age])
+    else:
+        print("Du bist zu jung für die Liste")
 
 file=open("data.csv")
 fileContent=file.read()
